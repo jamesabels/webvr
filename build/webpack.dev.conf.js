a812ebf2add7ea390d5830108e8ad312aa3bd112
James Abels <jamesrabels@gmail.com>
2016-12-02 23:03:58 -0500

    Add browsersync

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -4,6 +4,11 @@ var merge = require('webpack-merge')
 var utils = require('./utils')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+
+var bundler = webpack(baseWebpackConfig);
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -29,6 +34,14 @@ module.exports = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true
+    }),
+     new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://localhost:8080/'
+    },
+    {
+        reload: false
     })
   ]
 })
