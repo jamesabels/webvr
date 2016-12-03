a846312d87a3350f821178490df896599333ed0b
James Abels <jamesrabels@gmail.com>
2016-12-03 12:35:46 -0500

    Hello world VR

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
+    <a-scene>
+      <a-sky color="lightblue"></a-sky>
+
+      <a-entity position="0 2.82 -6.91" camera="active:false;userHeight:1.6;near:0.01" wasd-controls="" rotation="0 0 0" look-controls="" aframe-injected="" data-aframe-inspector="default-camera"></a-entity>
+
+      <!--Box-->
+      <a-entity 
+        data-v-641b6402="" 
+        position="0.08 2.78 -10.71" 
+        geometry="primitive:box;depth:1" 
+        material="color:salmon;opacity:0.8">
+      </a-entity>
+
+      <!--Light-->
+      <a-entity 
+        light="color:#FFF;intensity:0.10" 
+        position="0.03 5.21 -10.17" 
+        data-aframe-default-light="" 
+        aframe-injected="" 
+        rotation="8.021409131831525 0 0">
+      </a-entity>
+    
+    </a-scene>    
+
 </template>
 
 <script>
+require('aframe')
+
 export default {
   name: 'hello',
   data () {
